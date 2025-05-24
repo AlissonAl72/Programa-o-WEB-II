4e76af54b03629f4618f3a2171e431bfc7a46012
Alisson Almeida <141298032+AlissonAl72@users.noreply.github.com>
2025-05-23 20:09:30 -0400

    Add files via upload

---
+++ TWTodos/Object Repository/Page_TWTodos - Lista de Tarefas/a_Nova tarefa.rs
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Nova tarefa</name>
+   <tag></tag>
+   <elementGuidId>52bb1c2c-ad73-4379-9140-b9114a8a82a8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Nova tarefa')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>main.container > a.btn.btn-primary</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Nova tarefa&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>d833d5f4-0ac0-4a02-862f-120df8c0bab9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/create</value>
+      <webElementGuid>efc95ca4-7f25-4bd2-aae2-35ecd3405007</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-primary</value>
+      <webElementGuid>07db23b6-7938-4a97-9a83-28af77af8e54</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Nova tarefa</value>
+      <webElementGuid>2e02d7ca-bd2c-43ac-99d3-32bb4bd8444d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/main[@class=&quot;container&quot;]/a[@class=&quot;btn btn-primary&quot;]</value>
+      <webElementGuid>c8f8e261-2617-445f-824e-8f9b05dfaedd</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Nova tarefa')]</value>
+      <webElementGuid>740fc430-c6ee-4d61-9238-6ffc16c2ac58</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Excluír'])[1]/following::a[1]</value>
+      <webElementGuid>5a926fe6-9be9-4c89-89f0-6ec8986e71a0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Editar'])[1]/following::a[2]</value>
+      <webElementGuid>772b3aa0-6ce5-4ee0-b3af-4c8a602bfb31</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Nova tarefa']/parent::*</value>
+      <webElementGuid>4322711c-b732-45ab-a507-2d9bfa7446ae</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/create')]</value>
+      <webElementGuid>545fd909-32c5-495f-a984-37468c3766ee</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//main/a</value>
+      <webElementGuid>4a514b68-0d47-4e6d-ac65-3fda613cdf01</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/create' and (text() = 'Nova tarefa' or . = 'Nova tarefa')]</value>
+      <webElementGuid>bdc3420e-b828-4db8-8195-6ef8f1c96b59</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
